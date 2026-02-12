57f0f27351d3f3af6a2ab51c0c9d39bd9efd507e
Anurag Prajapati <161372177+Anurag-Prajapati12@users.noreply.github.com>
2026-02-13 00:47:04 +0530

    initial chatbot with ui

---
+++ langgraph_backend.py
@@ -0,0 +1,35 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage,HumanMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv 
+from langgraph.checkpoint.memory import MemorySaver
+
+# llm 
+load_dotenv()
+llm = ChatGoogleGenerativeAI(model="models/gemini-2.5-flash")
+
+# State
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+# Task
+def chat_node(state: ChatState):
+
+    messages = state['messages']
+
+    response = llm.invoke(messages)
+
+    return {'messages' : [response]}
+
+# Graph
+checkpointer = MemorySaver()
+graph = StateGraph(ChatState)
+
+graph.add_node('chat_node',chat_node)
+
+graph.add_edge(START,'chat_node')
+graph.add_edge('chat_node',END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
