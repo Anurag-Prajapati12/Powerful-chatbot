726e88903541d5a6bb0bc1df243c757eb6fda41d
Anurag Prajapati <161372177+Anurag-Prajapati12@users.noreply.github.com>
2026-02-15 12:47:50 +0530

    streaming

--- langgraph_backend.py
+++ langgraph_backend.py
@@ -1,10 +1,10 @@
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
-from langchain_core.messages import BaseMessage,HumanMessage
+from langchain_core.messages import BaseMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langgraph.graph.message import add_messages
 from dotenv import load_dotenv 
-from langgraph.checkpoint.memory import MemorySaver
+from langgraph.checkpoint.memory import InMemorySaver
 
 # llm 
 load_dotenv()
@@ -24,7 +24,7 @@ def chat_node(state: ChatState):
     return {'messages' : [response]}
 
 # Graph
-checkpointer = MemorySaver()
+checkpointer = InMemorySaver()
 graph = StateGraph(ChatState)
 
 graph.add_node('chat_node',chat_node)
@@ -33,3 +33,5 @@ graph.add_edge(START,'chat_node')
 graph.add_edge('chat_node',END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
+
+
